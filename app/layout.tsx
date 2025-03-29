15bcea31a3627f51b925352d455bf78d214f3002
Daniele Citran <daniele.ctr.zh@icloud.com>
2025-03-29 01:57:12 +0100

    adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,30 +3,26 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import Header from "./components/header"
 import Footer from "./components/footer"
-import SmoothScroll from "./components/SmoothScroll"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-    title: "Daniele Citran - Portfolio",
-    description: "Portfolio von Daniele Citran",
+    title: "Portfolio",
+    description: "Mein Portfolio",
 }
 
 export default function RootLayout({
-                                       children,
-                                   }: {
+    children,
+}: {
     children: React.ReactNode
 }) {
     return (
         <html lang="de">
-        <body className={`${inter.className} flex flex-col min-h-screen`}>
-        <SmoothScroll />
-        <Header />
-
-        <main className="flex-grow pt-16 sm:pt-14">{children}</main>
-
-        <Footer />
-        </body>
+            <body className={`${inter.className} flex flex-col min-h-screen`}>
+                <Header />
+                <main className="flex-grow pt-16 sm:pt-14">{children}</main>
+                <Footer />
+            </body>
         </html>
     )
 }
